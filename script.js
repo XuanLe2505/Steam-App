7dae8250bc1b595700d02edd4b9982a6c05535ea
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-03-20 17:35:40 +0800

    getGame and renderlist

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const getGames = async (searchQuery = "") => {
+    try{
+        const response = await fetch('https://cs-steam-api.herokuapp.com/games?q=${searchQuery}')
+        const data = await response.json()
+        return data
+    } catch(error){
+        console.log(error)
+    }
+};
+
+
+const renderGameList = async () => {
+    const games = await getGames();
+    ul.textContent = "";
+    games.forEach((game) => {
+        const li = document.createElement("list")
+        li.textContent = game.name;
+        ul.appendChild(li);
+    });
+}
+
+renderGameList();
+
+
+
+
