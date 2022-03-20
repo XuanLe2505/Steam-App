ef7377e74ab9e778ccb19c0592ed5dbcef1119d0
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-03-20 17:37:37 +0800

    fix

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 const getGames = async (searchQuery = "") => {
     try{
-        const response = await fetch('https://cs-steam-api.herokuapp.com/games?q=${searchQuery}')
-        const data = await response.json()
-        return data
+        const response = await fetch('https://cs-steam-api.herokuapp.com/games?q=${searchQuery}');
+        const data = await response.json();
+        return data;
     } catch(error){
-        console.log(error)
+        console.log(error);
     }
 };
 
 
-const renderGameList = async () => {
+const renderList = async () => {
     const games = await getGames();
     ul.textContent = "";
     games.forEach((game) => {
